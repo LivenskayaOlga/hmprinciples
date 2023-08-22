e7b7c228b42d8eac7d49eb96ae3d1253cba5ecef
Livenskaya Olga <livenskaya@list.ru>
2023-08-22 20:04:46 +0300

    Add class Post, class FormDate

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    //FormDate formDate = new FormDate();
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+
+
+}
